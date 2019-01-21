9688b00e6c8a6f333a59c66f669206f96c3ef199
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-21 19:29:59 +0100

    test seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,33 +1,21 @@
 require 'uri'
 
+headers = {
+
+    'Host'=>'www.google.com',
+    'User-Agent'=>'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:64.0) Gecko/20100101 Firefox/64.0',
+    'Accept'=>'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language'=>'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+    'Accept-Encoding'=>'gzip, deflate, br',
+    'DNT'=>'1',
+    'Connection'=>'keep-alive',
+    'Upgrade-Insecure-Requests'=>'1'
+
+}
 pages << {
     page_type: 'products_listing',
     method: 'GET',
-    url: "https://shop.coles.com.au/online/a-national/drinks/soft-drinks-3314551/energy-drinks?tabType=everything&tabId=everything&personaliseSort=false&orderBy=20601_6&errorView=AjaxActionErrorResponse&requesttype=ajax&beginIndex=0",
+    url: "https://www.google.com/search?q=Holiday+Inn+Birmingham+Airport%2C+5000+Richard+Arrington+Jr+Blvd+N%2C+Birmingham%2C+AL",
     ua_type: "gb2",
-    vars: {
-        'input_type' => 'taxonomy',
-        'search_term' => '-',
-        'page' => 1
-    }
-
-
+    headers:headers
 }
-search_terms = ['Red Bull', 'RedBull', 'Energidryck', 'Energidrycker']
-search_terms.each do |search_term|
-
-  pages << {
-      page_type: 'products_listing',
-      method: 'GET',
-      url: "https://shop.coles.com.au/online/COLRSSearchDisplay?storeId=20601&catalogId=10576&searchTerm=#{URI.encode(search_term)}&categoryId=&tabType=everything&tabId=everything&personaliseSort=false&langId=-1&beginIndex=0&browseView=false&facetLimit=100&searchSource=Q&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&pageView=image&errorView=AjaxActionErrorResponse&requesttype=ajax",
-      ua_type: "gb2",
-      vars: {
-          'input_type' => 'search',
-          'search_term' => search_term,
-          'page' => 1
-      }
-
-
-  }
-
-end
