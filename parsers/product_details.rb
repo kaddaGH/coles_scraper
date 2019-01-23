e1d129bc91f6dfd68bc49f1cb62650151186067a
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-01-23 01:56:33 +0100

    Update parser.

--- parsers/product_details.rb
+++ parsers/product_details.rb
@@ -37,7 +37,7 @@ if data
       /(\d*[\.,]?\d+)\s?([Tt]ablets)/,
       /(\d*[\.,]?\d+)\s?([Tt]ubes)/,
       /(\d*[\.,]?\d+)\s?([Cc]hews)/
-  ].find { |regexp| (title + ' ' + data['a']['O3'].first + ' ' + (data['a']['S5'].first rescue '')) =~ regexp }
+  ].find {|regexp| (title + ' ' + data['a']['O3'].first + ' ' + (data['a']['S5'].first rescue '')) =~ regexp}
   item_size = $1
   uom = $2
 
@@ -50,39 +50,40 @@ if data
       /(\d+)\s?[Cc][Tt]/,
       /(\d+)[\s-]?[Pp]ack($|[^e])/,
       /(\d+)\s?[Pp][Kk]/
-  ].find { |regexp| (title + ' ' + data['a']['O3'].first + ' ' + (data['a']['S5'].first rescue '')) =~ regexp }
+  ].find {|regexp| (title + ' ' + data['a']['O3'].first + ' ' + (data['a']['S5'].first rescue '')) =~ regexp}
   in_pack = match ? $1 : '1'
 
- info = {
-        # - - - - - - - - - - -
-        RETAILER_ID: '0017',
-        RETAILER_NAME: 'coles',
-        GEOGRAPHY_NAME: 'AU',
-        # - - - - - - - - - - -
-        SCRAPE_INPUT_TYPE:page['vars']['input_type'],
-        SCRAPE_INPUT_SEARCH_TERM:page['vars']['search_term'],
-        SCRAPE_INPUT_CATEGORY:page['vars']['input_type'] == 'taxonomy' ? category : '-',
-        SCRAPE_URL_NBR_PRODUCTS:page['vars']['SCRAPE_URL_NBR_PRODUCTS'],
-        # - - - - - - - - - - -
-        SCRAPE_URL_NBR_PROD_PG1:page['vars']['SCRAPE_URL_NBR_PRODUCTS'] ,
-        # - - - - - - - - - - -
-        PRODUCT_BRAND: brand,
-        PRODUCT_RANK:page['vars']['rank'],
-        PRODUCT_PAGE:page['vars']['page'],
-        PRODUCT_ID: id,
-        PRODUCT_NAME: title,
-        PRODUCT_DESCRIPTION: description,
-        PRODUCT_MAIN_IMAGE_URL: image_url,
-        PRODUCT_ITEM_SIZE: (item_size rescue ''),
-        PRODUCT_ITEM_SIZE_UOM: (uom rescue ''),
-        PRODUCT_ITEM_QTY_IN_PACK: (in_pack rescue ''),
-        PRODUCT_STAR_RATING: star_rating,
-        PRODUCT_NBR_OF_REVIEWS: number_of_reviews,
-        SALES_PRICE: price,
-        IS_AVAILABLE: is_available,
-        PROMOTION_TEXT: promotion_text,
-        EXTRACTED_ON: Time.now.to_s
-    }
- outputs << info
+  info = {
+      # - - - - - - - - - - -
+      RETAILER_ID: '0017',
+      RETAILER_NAME: 'coles',
+      GEOGRAPHY_NAME: 'AU',
+      # - - - - - - - - - - -
+      SCRAPE_INPUT_TYPE: page['vars']['input_type'],
+      SCRAPE_INPUT_SEARCH_TERM: page['vars']['search_term'],
+      SCRAPE_INPUT_CATEGORY: page['vars']['input_type'] == 'taxonomy' ? category : '-',
+      SCRAPE_URL_NBR_PRODUCTS: page['vars']['SCRAPE_URL_NBR_PRODUCTS'],
+      # - - - - - - - - - - -
+      SCRAPE_URL_NBR_PROD_PG1: page['vars']['SCRAPE_URL_NBR_PRODUCTS'],
+      # - - - - - - - - - - -
+      PRODUCT_BRAND: brand,
+      PRODUCT_RANK: page['vars']['rank'],
+      PRODUCT_PAGE: page['vars']['page'],
+      PRODUCT_ID: id,
+      PRODUCT_NAME: title,
+      PRODUCT_DESCRIPTION: description,
+      PRODUCT_MAIN_IMAGE_URL: image_url,
+      PRODUCT_ITEM_SIZE: (item_size rescue ''),
+      PRODUCT_ITEM_SIZE_UOM: (uom rescue ''),
+      PRODUCT_ITEM_QTY_IN_PACK: (in_pack rescue ''),
+      PRODUCT_STAR_RATING: star_rating,
+      PRODUCT_NBR_OF_REVIEWS: number_of_reviews,
+      SALES_PRICE: price,
+      IS_AVAILABLE: is_available,
+      PROMOTION_TEXT: promotion_text,
+      EXTRACTED_ON: Time.now.to_s
+  }
+  product_details['_collection'] = 'products'
+  outputs << info
 
-  end
+end
