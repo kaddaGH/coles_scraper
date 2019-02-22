d8fc3539b6e852f9cad3e1c120a1a2b50bcdebfa
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-22 12:46:03 +0100

    Update headers.

---
+++ lib/headers.rb
@@ -0,0 +1,19 @@
+module ReqHeaders
+  SEARCH_PAGE_HEADER_REQ = {
+      "Host" => "shop.coles.com.au",
+      "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:65.0) Gecko/20100101 Firefox/65.0",
+      "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Language" => "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3",
+      "Accept-Encoding" => "gzip, deflate, br",
+      "DNT" => "1",
+      "Connection" => "keep-alive",
+      "Cookie" => "KP_UID=ac5346967928f9f71870e869207f1b4c",
+      "Upgrade-Insecure-Requests" => "1",
+      "Pragma" => "no-cache",
+      "Cache-Control" => "no-cache"
+
+
+  }
+
+end
+
